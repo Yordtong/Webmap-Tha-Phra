b55861ccb5da083c623aec57cedf74c7e3915207
Yordtong <yordtong.d@kkumail.com>
2024-10-01 16:24:52 +0700

    Tha Phra

---
+++ data/_3.js
@@ -0,0 +1,1 @@
+var json__3 = {"type":"FeatureCollection","name":"_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"สถานที่":"เทศบาลตำบลท่าพระ","ลองติจูด":16.33008,"ละติจูด":102.79981},"geometry":{"type":"Point","coordinates":[102.79981,16.33008]}}]}
